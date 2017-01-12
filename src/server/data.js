b630c8320f6a13026c2aa28f46d8f85a27dc59e3
Tito Agudelo <titoagudelo@Titos-MacBook-Pro.local>
2017-01-11 23:22:22 -0500

    update api  for products and categories

--- src/server/data.js
+++ src/server/data.js
@@ -1,5 +1,7 @@
 module.exports = {
-  people: getPeople()
+  people: getPeople(),
+  categories: getCategories(),
+  products: getProducts()
 };
 
 function getPeople() {
@@ -12,5 +14,159 @@ function getPeople() {
     { id: 6, firstName: 'Landon', lastName: 'Gates', age: 11, location: 'South Carolina' },
     { id: 7, firstName: 'Haley', lastName: 'Guthrie', age: 35, location: 'Wyoming' },
     { id: 8, firstName: 'Aaron', lastName: 'Jinglehiemer', age: 22, location: 'Utah' }
+  ];
+}
+
+function getCategories() {
+  return [
+    {
+      categori_id: 1,
+      name: 'drinks'
+    },
+    {
+      categori_id: 2,
+      name: 'lunch'
+    },
+    {
+      categori_id: 3,
+      name: 'food'
+    },
+    {
+      categori_id: 4,
+      name: 'sea'
+    }
+  ]
+}
+
+function getProducts() {
+  return [
+    {
+      id: 1,
+      name: 'Lorem',
+      price: '60.000',
+      available: true,
+      best_seller: true,
+      categories: [
+        1,
+        4
+      ],
+      img: 'http://lorempixel.com/200/100/food/',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    },
+    {
+      id: 2,
+      name: 'ipsum',
+      price: '20.000',
+      available: false,
+      best_seller: false,
+      categories: [
+        4
+      ],
+      img: 'http://lorempixel.com/200/100/food/',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    },
+    {
+      id: 3,
+      name: 'dolor',
+      price: '10.000',
+      available: true,
+      best_seller: true,
+      categories: [
+        4
+      ],
+      img: 'http://lorempixel.com/200/100/food/',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    },
+    {
+      id: 4,
+      name: 'sit',
+      price: '35.000',
+      available: false,
+      best_seller: false,
+      categories: [
+        1,
+        2
+      ],
+      img: 'http://lorempixel.com/200/100/food/',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    },
+    {
+      id: 5,
+      name: 'amet',
+      price: '12.000',
+      available: true,
+      best_seller: true,
+      categories: [
+        1,
+        4
+      ],
+      img: 'http://lorempixel.com/200/100/food/',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    },
+    {
+      id: 6,
+      name: 'consectetur',
+      price: '120.000',
+      available: true,
+      best_seller: false,
+      categories: [
+        1,
+        4
+      ],
+      img: 'http://lorempixel.com/200/100/food/',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    },
+    {
+      id: 7,
+      name: 'adipiscing',
+      price: '50.000',
+      available: false,
+      best_seller: false,
+      categories: [
+        1,
+        3
+      ],
+      img: 'http://lorempixel.com/200/100/food/',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    },
+    {
+      id: 8,
+      name: 'elit',
+      price: '2000',
+      available: true,
+      best_seller: false,
+      categories: [
+        1,
+        3
+      ],
+      img: 'http://lorempixel.com/200/100/food/',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    },
+    {
+      id: 9,
+      name: 'Maecenas',
+      price: '150.000',
+      available: true,
+      best_seller: true,
+      categories: [
+        2,
+        4
+      ],
+      img: 'http://lorempixel.com/200/100/food/',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    },
+    {
+      id: 10,
+      name: 'eu',
+      price: '200.000',
+      available: false,
+      best_seller: true,
+      categories: [
+        2,
+        3
+      ],
+      img: 'http://lorempixel.com/200/100/food/',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.'
+    }
   ];
 }
--- src/server/routes.js
+++ src/server/routes.js
@@ -4,6 +4,10 @@ var data = require('./data');
 
 router.get('/people', getPeople);
 router.get('/person/:id', getPerson);
+router.get('/products', getProducts);
+router.get('/products/:id', getProduct);
+router.get('/categories', getCategories);
+router.get('/categories/:id', getCategory);
 router.get('/*', four0four.notFoundMiddleware);
 
 module.exports = router;
@@ -26,3 +30,38 @@ function getPerson(req, res, next) {
     four0four.send404(req, res, 'person ' + id + ' not found');
   }
 }
+
+function getProducts(req, res, next) {
+  res.status(200).send(data.products);
+}
+
+function getProduct(req, res, next) {
+  var id = +req.params.id;
+  var product = data.products.filter(function(p) {
+    return p.id === id;
+  })[0];
+
+  if (product) {
+    res.status(200).send(product);
+  } else {
+    four0four.send404(req, res, 'product ' + id + ' not found');
+  }
+}
+
+function getCategories(req, res, next) {
+  res.status(200).send(data.categories);
+}
+
+function getCategory(req, res, next) {
+  var id = +req.params.id;
+  var category = data.categories.filter(function(p) {
+    return p.id === id;
+  })[0];
+
+  if (category) {
+    res.status(200).send(category);
+  } else {
+    four0four.send404(req, res, 'product ' + id + ' not found');
+  }
+}
+
