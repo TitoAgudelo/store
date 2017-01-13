67135d6cc7402a5f8e7cd6104887c1001e3e3f99
Tito Agudelo <titoagudelo@Titos-MacBook-Pro.local>
2017-01-12 23:53:22 -0500

    update categories on product list

--- src/client/app/admin/admin.controller.js
+++ src/client/app/admin/admin.controller.js
@@ -22,6 +22,31 @@
       });
     }
 
+    function getCategory(products) {
+      if(vm.categories.length > 0) {
+        angular.forEach(products, function(value, key) {
+          value.categoryObject = getCategoryInfo(value.categories);
+        });
+      } else {
+        getCategories();
+      }
+    }
+
+    function getCategoryInfo(categoryArray) {
+      var categories = [];
+      if(categoryArray) {
+        categoryArray.forEach(function(value, key) {
+          angular.forEach(vm.categories, function(val, key) {
+            if(val.categori_id === value) {
+              categories.push(val);
+            }
+          })
+        });
+
+        return categories;
+      }
+    }
+
     function getCategories() {
       return dataservice.getCategories().then(function(data) {
         vm.categories = data;
@@ -32,6 +57,7 @@
     function getProducts() {
       return dataservice.getProducts().then(function(data) {
         vm.products = data;
+        getCategory(vm.products);
         return vm.products;
       });
     }
